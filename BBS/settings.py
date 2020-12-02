3b4e819ee9cfd1c335376a57fcb820960e92f6b4
娄锋 <shs-xxaqbzylf2@chinaunicom.cn>
2020-12-02 14:39:45 +0800

    add .gitattributes

--- BBS/settings.py
+++ BBS/settings.py
@@ -79,8 +79,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'bbs',
-        'USER': 'root',
-        'PASSWORD': 'root',
+        'USER': 'xxxxxx',
+        'PASSWORD': 'xxxxxx',
         'HOST': '127.0.0.1',
         'PORT': 3306,
         'CHARSET': 'utf8',
